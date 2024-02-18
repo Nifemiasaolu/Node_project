bb96a961dfbb2002094aa8ee623207c9aefc4ea8
Nifemi Asaolu <theophilusasaolu@gmail.com>
2024-02-18 22:57:59 +0100

    Update on details

---
+++ 9-fs-sync.js
@@ -0,0 +1,15 @@
+// SYNCHRONOUS
+
+const {readFileSync, writeFileSync,} = require('fs')
+console.log("start")
+const first = readFileSync('./content/first.txt', 'utf8')
+const second = readFileSync('./content/second.txt', 'utf8')
+
+// console.log(first, second)
+
+// writeFileSync is used to create new file.
+writeFileSync('./content/result-sync.txt', `This is the result: ${first} \n ${second}`);
+console.log("Done with the task")
+console.log("Starting the next one")
+
+// ////
